59d8b3826061b73021614094d74010524cc6f080
juniorgomesk <156450127+juniorgomesk@users.noreply.github.com>
2026-04-15 15:06:05 -0400

    Add files via upload

---
+++ database_links.js
@@ -0,0 +1,15 @@
+// Este é o seu banco de dados de links. 
+// Para adicionar novos, basta seguir o padrão: { nome: "NOME", url: "LINK" },
+const dbLinks = [
+    { nome: "SIGED", url: "https://siged.amazonas.am.gov.br/siged/login" },
+    { nome: "SIGED ALTERNATIVO", url: "https://sistemas.sefaz.am.gov.br/siged/login" },
+    { nome: "PRODAM-RH", url: "https://prodamrh.prodam.am.gov.br/" },
+    { nome: "OS2PDF", url: "https://os2pdf.prodam.am.gov.br/os2pdf/index.php" },
+    { nome: "SISCCOM", url: "http://10.46.22.175:81/sisccom/menu_lateral" },
+    { nome: "CHAMADOS", url: "https://www.chamadossgrh.site/pagina_inicial.php" },
+    { nome: "SACP PRODAM", url: "https://sac.prodam.am.gov.br/index.php/usuarios/login" },
+    { nome: "DIÁRIO OFICIAL", url: "https://diario.imprensaoficial.am.gov.br/" },
+    { nome: "PONTO CELULAR", url: "https://pontosead.site/" },
+    { nome: "PONTO COMPUTADOR", url: "http://10.46.22.175:81/ponto%20sead/" },
+    { nome: "CRONOGRAMA FOLHA", url: "https://chamadossgrh.site/cronograma_folha/calendario.php" }
+];
