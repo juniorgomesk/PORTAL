d1c5f1f209d41bbc4228f94e56baa8e5793e0b21
juniorgomesk <156450127+juniorgomesk@users.noreply.github.com>
2026-04-16 11:04:26 -0400

    Add files via upload

---
+++ database_logins.js
@@ -0,0 +1,6 @@
+// Este é o seu banco de dados de logins. 
+// Para adicionar novos, basta seguir o padrão.
+const dbLogins = [
+    { usuario: "junior", senha: "123" },
+    { usuario: "teste", senha: "123" }
+];
